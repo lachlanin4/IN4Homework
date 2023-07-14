cc7fdfb5fa91759b4b1f886fef1c4201d0dbe63a
lachlanin4 <138771039+lachlanin4@users.noreply.github.com>
2023-07-14 10:15:37 +0100

    Add some comments to the test class

--- TextBasedAdventGame/character_test.py
+++ TextBasedAdventGame/character_test.py
@@ -3,13 +3,25 @@
 from character import Character
 from enemy import Enemy
 
+# Create character called dave
 dave = Character("Dave", "A smelly zombie")
+
+# Print out daves discription
 dave.describe()
+
+# Set daves conversation
 dave.set_conversation("Hello I'm Dave and I'm new here")
+
+# Make dave talk
 dave.talk()
 
+# Create Enemy named "bork"
 bork = Enemy("bork", "A smelly zombie")
+
+# Set a weakness
 bork.set_weakness("cheese")
+
+# Fight with dave
 print("What will you fight with?")
 fight_with = input()
 bork.fight(fight_with)
